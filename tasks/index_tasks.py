9cea5f689a32b86bacfbbea810e56c8f4c8b913a
持羽静尘 <reinhard7717@hotmail.com>
2025-03-31 18:34:06 +0800

    fixbug

--- tasks/index_tasks.py
+++ tasks/index_tasks.py
@@ -50,43 +50,29 @@ async def save_index_all_latest_time_series(self): # 添加 async def
     logger.info("保存指数时间序列数据完成")
     return "保存指数时间序列数据完成"
 
-@celery_app.task(bind=True, name='tasks.index_tasks.save_index_history_time_series_chunk')
-async def save_index_history_time_series_chunk(self, index_chunk):
-    """
-    保存指数历史时间序列数据块
-    """
-    from dao_manager.daos.index_dao import StockIndexDAO
-    index_dao = StockIndexDAO()
-    logger.info(f"开始保存指数历史时间序列数据块: {[index.code for index in index_chunk]}") # 打印指数代码，方便查看日志
-    await index_dao.fetch_and_save_history_time_series_for_indexes(index_chunk) # 修改方法名并确保 await
-    logger.info(f"保存指数历史时间序列数据块完成: {[index.code for index in index_chunk]}")
-    return f"保存指数历史时间序列数据块完成: {[index.code for index in index_chunk]}"
+# @celery_app.task(bind=True, name='tasks.index_tasks.save_index_history_time_series_chunk')
+# async def save_index_history_time_series_chunk(self, index_chunk):
+#     """
+#     保存指数历史时间序列数据块
+#     """
+#     from dao_manager.daos.index_dao import StockIndexDAO
+#     index_dao = StockIndexDAO()
+#     logger.info(f"开始保存指数历史时间序列数据块: {[index.code for index in index_chunk]}") # 打印指数代码，方便查看日志
+#     await index_dao.fetch_and_save_history_time_series_for_indexes(index_chunk) # 修改方法名并确保 await
+#     logger.info(f"保存指数历史时间序列数据块完成: {[index.code for index in index_chunk]}")
+#     return f"保存指数历史时间序列数据块完成: {[index.code for index in index_chunk]}"
 
 @celery_app.task(bind=True, name='tasks.index_tasks.save_index_all_history_time_series')
 async def save_index_all_history_time_series(self):
     """
-    保存指数历史时间序列数据(并行版本)
+    保存指数历史时间序列数据 (简化版 - 无并行)
     """
     from dao_manager.daos.index_dao import StockIndexDAO
     index_dao = StockIndexDAO()
-    logger.info("开始并行保存指数历史时间序列数据")
-
-    indexes = await index_dao.get_all_indexes() # 获取所有指数对象列表
-    if not indexes:
-        logger.info("没有需要更新历史时间序列数据的指数")
-        return "没有需要更新历史时间序列数据的指数"
-
-    chunk_size = len(indexes) // 8 + 1  # 分割成 8 块，可以根据实际情况调整
-    index_chunks = chunk_list(indexes, chunk_size) # 使用 chunk_list 分割指数对象列表
-
-    tasks = [save_index_history_time_series_chunk.s(chunk) for chunk in index_chunks] # 创建子任务签名
-    job = group(tasks) # 创建任务组
-    async_result = job.apply_async() # 异步执行任务组
-
-    # async_result.get() # 如果需要等待所有子任务完成，可以调用 get()，否则可以省略
-
-    logger.info("并行保存指数历史时间序列数据任务调度完成，子任务正在后台执行")
-    return "并行保存指数历史时间序列数据任务调度完成，子任务正在后台执行"
+    logger.info("开始保存指数历史时间序列数据 (简化版)")
+    await index_dao.fetch_and_save_all_history_time_series() # 直接调用原始方法
+    logger.info("保存指数历史时间序列数据完成 (简化版)")
+    return "保存指数历史时间序列数据完成 (简化版)"
 
 @celery_app.task(bind=True, name='tasks.index_tasks.save_index_all_latest_kdj')  # 使用 @celery_app.task 装饰器，并指定任务名称
 async def save_index_all_latest_kdj(self): # 添加 async def
