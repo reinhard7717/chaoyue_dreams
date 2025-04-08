b06730f59614f3c32b6ce8a330ca8ff9ecb474a9
持羽静尘 <reinhard7717@hotmail.com>
2025-04-08 14:02:05 +0800

    fixbug

--- dao_manager/daos/stock_indicators_dao.py
+++ dao_manager/daos/stock_indicators_dao.py
@@ -236,31 +236,31 @@ class StockIndicatorsDAO(BaseDAO):
             logger.warning(f"股票代码[{stock_code}]不存在，无法获取时间序列数据")
             return {'创建': 0, '更新': 0, '跳过': 0}
         data_dicts = []
-        for time_level in TIME_LEVELS:
-            try:
+        try:
+            for time_level in TIME_LEVELS:
                 api_data = await self.api.get_time_trade(stock.stock_code, time_level)
+                if api_data.get('trade_time') is None:
+                    logger.warning(f"API未返回{stock} {time_level}级别时间序列数据")
+                    return {'创建': 0, '更新': 0, '跳过': 0}
                 data_dict = self.data_format_process.set_time_trade_data(stock, time_level, api_data)
                 data_dicts.append(data_dict)
                 cache_dict = data_dict.copy()
                 await self.cache_set.latest_time_trade(stock.stock_code, time_level, cache_dict)
-                await self.cache_set.history_time_trade(stock.stock_code, time_level, cache_dict)
-            except Exception as e:
-                logger.error(f"获取{stock}股票{time_level}级别时间序列数据出错111111: {str(e)}")    
-            # --- 生成缓存键 ---
-            cache_key =  self.cache_key.history_time_trade(stock_code, time_level)
-            # --- 单行调用修剪方法 ---
-            await self.cache_manager.trim_cache_zset(cache_key, self.cache_limit)
-            # --- 修剪调用结束 ---
-        if not data_dicts:
-            logger.warning(f"API未返回{stock} {time_level}级别时间序列数据")
-            return {'创建': 0, '更新': 0, '跳过': 0}
-        # 保存数据
-        result = await self._save_all_to_db_native_upsert(
-            model_class=StockTimeTrade,
-            data_list=data_dicts,
-            unique_fields=['stock', 'time_level', 'trade_time']
-        )
-        try:
+                await self.cache_set.history_time_trade(stock.stock_code, time_level, cache_dict)  
+                # --- 生成缓存键 ---
+                cache_key =  self.cache_key.history_time_trade(stock_code, time_level)
+                # --- 单行调用修剪方法 ---
+                await self.cache_manager.trim_cache_zset(cache_key, self.cache_limit)
+                # --- 修剪调用结束 ---
+            if not data_dicts:
+                logger.warning(f"API未返回{stock} {time_level}级别时间序列数据")
+                return {'创建': 0, '更新': 0, '跳过': 0}
+            # 保存数据
+            result = await self._save_all_to_db_native_upsert(
+                model_class=StockTimeTrade,
+                data_list=data_dicts,
+                unique_fields=['stock', 'time_level', 'trade_time']
+            )
             logger.info(f"{stock} 股票分时成交数据保存完成，结果: {result}")
             return result
         except Exception as e:
