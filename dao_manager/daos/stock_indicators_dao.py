99da5e34bdc1b16fd4ad4a39218ea06de611d59c
持羽静尘 <reinhard7717@hotmail.com>
2025-04-11 14:08:52 +0800

    fixbug

--- dao_manager/daos/stock_indicators_dao.py
+++ dao_manager/daos/stock_indicators_dao.py
@@ -241,12 +241,12 @@ class StockIndicatorsDAO(BaseDAO):
                 api_data = await self.api.get_time_trade(stock.stock_code, time_level)
                 if isinstance(api_data, dict):
                     data_dict = self.data_format_process.set_time_trade_data(stock, time_level, api_data)
-                    if data_dict.get('trade_time') is None:
+                    if data_dict.get('d') is None:
                         logger.warning(f"API未返回{stock} {time_level}级别时间序列数据")
                         return {'创建': 0, '更新': 0, '跳过': 0}
                     cache_dict = data_dict.copy()
                     data_dicts.append(data_dict)
-                    logger.info(f"api_data: {api_data.get('d')}, cache_dict.date: {cache_dict.get('trade_time')}")
+                    # logger.info(f"api_data: {api_data.get('d')}, cache_dict.date: {cache_dict.get('trade_time')}")
                     await self.cache_set.latest_time_trade(stock.stock_code, time_level, cache_dict)
                     await self.cache_set.history_time_trade(stock.stock_code, time_level, cache_dict) 
                     # --- 生成缓存键 ---
