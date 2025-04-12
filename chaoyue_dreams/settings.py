c66a05d40c17f1b909fb84e5437ea465d9175a0b
持羽静尘 <reinhard7717@hotmail.com>
2025-04-12 14:16:12 +0800

    fixbug

--- chaoyue_dreams/settings.py
+++ chaoyue_dreams/settings.py
@@ -35,19 +35,17 @@ def get_local_ip():
     return ip
 
 SERVER_IP = get_local_ip()
-print(f"本地IP: {get_local_ip()}")  # 输出您的本地IP
-print(f"动态设置的SERVER_IP: {SERVER_IP}")  # 输出动态设置的SERVER_IP
 TARGET_SERVER_IP = "39.101.65.133"
 REDIS_PASSWORD = 'Asdf1234' # 将密码定义在这里，方便复用
 
-if SERVER_IP == TARGET_SERVER_IP:
+if SERVER_IP == TARGET_SERVER_IP or SERVER_IP == '172.30.93.156':
     REDIS_HOST_DYNAMIC = '127.0.0.1'
     MYSQL_HOST_DYNAMIC = '127.0.0.1'
-    # print(f"检测到服务器IP为 {SERVER_IP}，Redis Host 设置为: 127.0.0.1")
+    print(f"检测到服务器IP为 {SERVER_IP}，Redis Host 设置为: 127.0.0.1")
 else:
     REDIS_HOST_DYNAMIC = TARGET_SERVER_IP
     MYSQL_HOST_DYNAMIC = TARGET_SERVER_IP
-    # print(f"检测到服务器IP为 {SERVER_IP} (非 {TARGET_SERVER_IP})，Redis Host 设置为: {TARGET_SERVER_IP}")
+    print(f"检测到服务器IP为 {SERVER_IP} (非 {TARGET_SERVER_IP})，Redis Host 设置为: {TARGET_SERVER_IP}")
 
 # --- 结束: 动态获取本机IP并设置Redis主机 ---
 
