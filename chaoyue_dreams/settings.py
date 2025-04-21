07bb63d86b298665cdd4cb8f541622f4f4d7d32b
持羽静尘 <reinhard7717@hotmail.com>
2025-04-21 14:27:56 +0800

    fixbug

--- chaoyue_dreams/settings.py
+++ chaoyue_dreams/settings.py
@@ -735,30 +735,29 @@ CELERY_BEAT_SCHEDULE = {
         'task': 'tasks.stock_time_trade_tasks.save_latest_trade_datas_by_time_level', # 任务函数名
         'schedule': crontab(minute='*/5', hour='9,10,11,13,14', day_of_week='mon,tue,wed,thu,fri'), # 交易时段每 5 分钟执行
         'kwargs': {'time_level': '5'},
+        'options': {'queue': 'save_api_data_TimeTrade'},  # 添加此行：指定队列名称
     },
     '每 15 分钟运行一次所有股票的K线数据获取任务': {
         # 这里包含了获得最新数据、计算指标、执行策略等步骤
         'task': 'tasks.stock_time_trade_tasks.save_latest_trade_datas_by_time_level', # 任务函数名
         'schedule': crontab(minute='*/15', hour='9,10,11,13,14', day_of_week='mon,tue,wed,thu,fri'), # 交易时段每 5 分钟执行
         'kwargs': {'time_level': '15'},
+        'options': {'queue': 'save_api_data_TimeTrade'},  # 添加此行：指定队列名称
     },
     '每 30 分钟运行一次所有股票的K线数据获取任务': {
         # 这里包含了获得最新数据、计算指标、执行策略等步骤
         'task': 'tasks.stock_time_trade_tasks.save_latest_trade_datas_by_time_level', # 任务函数名
         'schedule': crontab(minute='*/30', hour='9,10,11,13,14', day_of_week='mon,tue,wed,thu,fri'), # 交易时段每 5 分钟执行
         'kwargs': {'time_level': '30'},
+        'options': {'queue': 'save_api_data_TimeTrade'},  # 添加此行：指定队列名称
     },
     '每 60 分钟运行一次所有股票的K线数据获取任务': {
         # 这里包含了获得最新数据、计算指标、执行策略等步骤
         'task': 'tasks.stock_time_trade_tasks.save_latest_trade_datas_by_time_level', # 任务函数名
         'schedule': crontab(minute='*/60', hour='9,10,11,13,14', day_of_week='mon,tue,wed,thu,fri'), # 交易时段每 5 分钟执行
         'kwargs': {'time_level': '60'},
+        'options': {'queue': 'save_api_data_TimeTrade'},  # 添加此行：指定队列名称
     },
-    # '每 5 分钟运行一次所有股票的指标计算任务': {
-    #     # 这里包含了获得最新数据、计算指标、执行策略等步骤
-    #     'task': 'tasks.calculate_tasks.calculate_stock_indicators', # 任务函数名
-    #     'schedule': crontab(minute='*/5', hour='9,10,11,13,14', day_of_week='mon,tue,wed,thu,fri'), # 交易时段每 5 分钟执行
-    # },
     '每 5 分钟运行一次所有股票的策略计算任务': {
         # 这里包含了获得最新数据、计算指标、执行策略等步骤
         'task': 'tasks.stock_analysis.analyze_all_stocks', # 任务函数名
