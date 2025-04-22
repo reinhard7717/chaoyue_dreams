791e71a7909c6f641095d15f7c0a19871457f1b6
持羽静尘 <reinhard7717@hotmail.com>
2025-04-22 22:40:02 +0800

    fixbug

--- stock_models/apps.py
+++ stock_models/apps.py
@@ -9,4 +9,4 @@ class StockModelsConfig(AppConfig):
     def ready(self):
         from . import stock_basic, stock_realtime, index, fund_flow, stock_indicators, stock_analytics
         from .datacenter import capital_flow, financial, institution, lhb, market_data, north_south, statistics
-        from .indicator import adl, atr, boll, cci, cmf, dmi, ichimoku, kc, kdj, ma, macd, mfi, mom, obv, pivot_points, roc, rsi, sar, sma, stochastic_oscillator, vroc, vwap, wr
+        # from .indicator import adl, atr, boll, cci, cmf, dmi, ichimoku, kc, kdj, ma, macd, mfi, mom, obv, pivot_points, roc, rsi, sar, sma, stochastic_oscillator, vroc, vwap, wr
