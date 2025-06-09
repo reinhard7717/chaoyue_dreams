501d43e4873c3754843c4535d96f8e63b5e6f39d
持羽静尘 <reinhard7717@hotmail.com>
2025-06-09 19:23:48 +0800

    更新URL路由配置，优化用户访问路径
    - 注释掉不必要的URL路径，保持代码整洁性。
    - 为用户相关和dashboard相关的URL添加命名空间，提升可读性。
    - 确保根路径重定向到dashboard，优化用户体验。

--- chaoyue_dreams/urls.py
+++ chaoyue_dreams/urls.py
@@ -6,11 +6,17 @@ from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
 urlpatterns = [
+    # path('admin/', admin.site.urls),
+    # path('users/', include('users.urls', namespace='users')),  # 用户相关URL
+    # path('dashboard/', include('dashboard.urls', namespace='dashboard')), # 包含 dashboard 的 URLs
+    # path('', include('django.contrib.auth.urls')), # 添加这行可以快速使用内置的登录/登出等URL name
+    # # 可以设置一个根路径重定向到 dashboard
+    # path('', RedirectView.as_view(url='/dashboard/', permanent=False), name='go-to-dashboard'),
+
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),  # 用户相关URL
-    path('dashboard/', include('dashboard.urls')), # 包含 dashboard 的 URLs
-    path('', include('django.contrib.auth.urls')), # 添加这行可以快速使用内置的登录/登出等URL name
-    # 可以设置一个根路径重定向到 dashboard
+    path('users/', include('users.urls', namespace='users')),  # 用户相关URL
+    path('dashboard/', include('dashboard.urls', namespace='dashboard')), # dashboard URLs
+    path('accounts/', include('django.contrib.auth.urls')), # 登录/登出等URL
     path('', RedirectView.as_view(url='/dashboard/', permanent=False), name='go-to-dashboard'),
     
     # path('tasks/', include('tasks.urls')),  # 任务管理URL
