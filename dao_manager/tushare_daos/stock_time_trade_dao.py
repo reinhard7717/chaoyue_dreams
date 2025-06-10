c8424b4108b5a26e52bf3f8edb1d3a71207b2023
持羽静尘 <reinhard7717@hotmail.com>
2025-06-10 16:12:14 +0800

    优化股票分钟交易数据处理逻辑
    - 在StockTimeTradeDAO中，移除冗余的打印语句，提升代码整洁性。
    - 仅在存在交易时间时才将数据字典添加到结果列表，确保数据的有效性和准确性。

--- dao_manager/tushare_daos/stock_time_trade_dao.py
+++ dao_manager/tushare_daos/stock_time_trade_dao.py
@@ -740,9 +740,7 @@ class StockTimeTradeDAO(BaseDAO):
                 stock = await self.stock_basic_dao.get_stock_by_code(row.ts_code)
                 if stock:
                     data_dict = self.data_format_process_trade.set_time_trade_minute_data(stock=stock, df_data=row)
-                    if not data_dict.get('trade_time'):
-                        print(f"data_dict: {data_dict}，row：{row}")
-                    else:
+                    if data_dict.get('trade_time'):
                         data_dicts.append(data_dict)
         if data_dicts:
             result = await self._save_all_to_db_native_upsert(
