25f9ada294918c06fc557f305ee6e17506c56f1e
持羽静尘 <reinhard7717@hotmail.com>
2025-07-29 13:24:43 +0800

    更新Celery任务配置，新增实时数据处理任务'save_stocks_minute_data_realtime_task-0940'，确保定时任务能够每分钟获取最新数据并计算指标，提升数据处理的实时性和准确性。

--- chaoyue_dreams/settings.py
+++ chaoyue_dreams/settings.py
@@ -479,6 +479,13 @@ CELERY_BEAT_SCHEDULE = {
         # 每天9:30-21:30，每5分钟执行一次
         'schedule': crontab( minute='*/5', hour='0-1' ),
     },
+    'save_stocks_minute_data_realtime_task-0940': {
+        # 这里包含了获得最新数据、计算指标、执行策略等步骤
+        'task': 'tasks.tushare.stock_realtime_tasks.save_stocks_minute_data_realtime_task', # 任务函数名
+        'schedule': crontab( minute='*1', hour='0-1' ),
+        'kwargs': {'time_level': '1'},
+        'options': {'queue': 'celery'},  # 添加此行：指定队列名称，这是调度器的队列
+    },
     'save_stocks_minute_data_realtime_task-0925': {
         # 这里包含了获得最新数据、计算指标、执行策略等步骤
         'task': 'tasks.tushare.stock_realtime_tasks.save_stocks_minute_data_realtime_task', # 任务函数名
