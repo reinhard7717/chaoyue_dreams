fce62f47fbfaab2661338339b2987b25430e5123
持羽静尘 <reinhard7717@hotmail.com>
2025-08-13 14:31:50 +0800

    fixbug

--- strategies/trend_following/intelligence/cognitive_intelligence.py
+++ strategies/trend_following/intelligence/cognitive_intelligence.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 from typing import Dict
+from enum import Enum
 from strategies.trend_following.utils import get_params_block, get_param_value
 
 class MainForceState(Enum):
