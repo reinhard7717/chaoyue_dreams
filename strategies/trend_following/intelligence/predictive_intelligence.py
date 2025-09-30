da8a4152082eefdd255fd4a4dbb988bcf86d0518
持羽静尘 <reinhard7717@hotmail.com>
2025-09-30 11:48:57 +0800

    fixbug

--- strategies/trend_following/intelligence/predictive_intelligence.py
+++ strategies/trend_following/intelligence/predictive_intelligence.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 from typing import Dict
-from ..utils.params import get_params_block, get_param_value
+from strategies.trend_following.utils import get_params_block, get_param_value
 
 class PredictiveIntelligence:
     """
