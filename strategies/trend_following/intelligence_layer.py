8bd8e8329d4b4d0acd03a83c27330640735cc083
持羽静尘 <reinhard7717@hotmail.com>
2025-10-07 10:59:13 +0800

    fixbug

--- strategies/trend_following/intelligence_layer.py
+++ strategies/trend_following/intelligence_layer.py
@@ -102,8 +102,8 @@ class IntelligenceLayer:
         exit_triggers_df = self.structural_defense_layer.generate_hard_exit_triggers()
         self.strategy.exit_triggers = exit_triggers_df
         debug_params = get_params_block(self.strategy, 'debug_params', {})
-        if get_param_value(debug_params.get('enabled'), False):
-            self.deploy_forensic_probes()
+        # if get_param_value(debug_params.get('enabled'), False):
+        #     self.deploy_forensic_probes()
         # print("--- [情报层总指挥官 V415.0] 所有诊断模块执行完毕。 ---")
         return self.strategy.trigger_events
 
