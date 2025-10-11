608d3155590509e00e1c765ce5544e78210de47d
持羽静尘 <reinhard7717@hotmail.com>
2025-10-11 11:57:56 +0800

    调试

--- strategies/trend_following/intelligence_layer.py
+++ strategies/trend_following/intelligence_layer.py
@@ -19,7 +19,7 @@ from strategies.kline_pattern_recognizer import KlinePatternRecognizer
 from .intelligence.pattern_intelligence import PatternIntelligence
 from .intelligence.process_intelligence import ProcessIntelligence
 from .intelligence.predictive_intelligence import PredictiveIntelligence
-from strategies.trend_following.utils import get_params_block, get_param_value, calculate_context_scores, normalize_score, normalize_to_bipolar, _calculate_gaia_bedrock_support, _calculate_historical_low_support
+from strategies.trend_following.utils import get_params_block, get_param_value, calculate_context_scores, normalize_score, normalize_to_bipolar, _calculate_gaia_bedrock_support, _calculate_historical_low_support, get_unified_score
 
 class IntelligenceLayer:
     """
