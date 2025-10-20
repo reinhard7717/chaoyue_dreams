b1d76687d8ff05e43d38f8444a14385a0a8e2eba
持羽静尘 <reinhard7717@hotmail.com>
2025-10-20 14:57:39 +0800

    fixbug

--- services/chip_feature_calculator.py
+++ services/chip_feature_calculator.py
@@ -578,19 +578,17 @@ class ChipFeatureCalculator:
         return results
 
     def _calculate_cross_day_chip_flow(self, context: dict) -> dict:
-        """【V1.2 - 上下文引用修正版】计算跨日筹码迁徙"""
+        """【V1.3 - 笔误修正版】计算跨日筹码迁徙"""
         results = {
             'short_term_profit_taking_ratio': None,
             'long_term_chips_unlocked_ratio': None,
             'short_term_capitulation_ratio': None,
             'long_term_despair_selling_ratio': None,
         }
-        # [代码修改开始] 修正上下文引用，从 self.ctx 改为 context
         prev_chips_df = context.get('prev_chip_distribution')
         prev_close = context.get('prev_close_price')
         prev_prev_20d_close = context.get('prev_prev_20d_close')
         daily_turnover_vol = context.get('daily_turnover_volume')
-        # [代码修改结束]
         is_data_invalid = False
         if prev_chips_df is None or prev_chips_df.empty:
             is_data_invalid = True
@@ -605,8 +603,10 @@ class ChipFeatureCalculator:
         prev_losers = prev_chips_df[prev_chips_df['price'] > prev_close]
         st_winners_pct = prev_winners[prev_winners['price'] >= prev_prev_20d_close]['percent'].sum()
         lt_winners_pct = prev_winners[prev_winners['price'] < prev_prev_20d_close]['percent'].sum()
-        st_losers_pct = prev_losers[losers_df['price'] >= prev_prev_20d_close]['percent'].sum()
-        lt_losers_pct = prev_losers[losers_df['price'] < prev_prev_20d_close]['percent'].sum()
+        # [代码修改开始] 修正笔误，将 losers_df 改为 prev_losers
+        st_losers_pct = prev_losers[prev_losers['price'] >= prev_prev_20d_close]['percent'].sum()
+        lt_losers_pct = prev_losers[prev_losers['price'] < prev_prev_20d_close]['percent'].sum()
+        # [代码修改结束]
         results['short_term_profit_taking_ratio'] = st_winners_pct
         results['long_term_chips_unlocked_ratio'] = lt_winners_pct
         results['short_term_capitulation_ratio'] = st_losers_pct
