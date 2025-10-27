1d31bc7556f6afe52a05385af79caf31a89f1547
持羽静尘 <reinhard7717@hotmail.com>
2025-10-27 09:51:06 +0800

    调试

--- strategies/trend_following/probes/cognitive_probes.py
+++ strategies/trend_following/probes/cognitive_probes.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from strategies.trend_following.utils import get_params_block, get_param_value, normalize_score
+from strategies.trend_following.utils import get_params_block, get_param_value, normalize_score, normalize_to_bipolar
 
 class CognitiveProbes:
     """
