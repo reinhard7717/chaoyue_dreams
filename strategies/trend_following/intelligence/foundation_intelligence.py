7429c494e63efb24910c0bb3a60b4b83b3c84081
持羽静尘 <reinhard7717@hotmail.com>
2025-10-28 22:30:09 +0800

    fixbug

--- strategies/trend_following/intelligence/foundation_intelligence.py
+++ strategies/trend_following/intelligence/foundation_intelligence.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import numpy as np
 from typing import Dict, Tuple
-from strategies.trend_following.utils import get_params_block, get_param_value, normalize_score, normalize_to_bipolar
+from strategies.trend_following.utils import get_params_block, get_param_value, normalize_score, normalize_to_bipolar, bipolar_to_exclusive_unipolar
 
 class FoundationIntelligence:
     def __init__(self, strategy_instance):
