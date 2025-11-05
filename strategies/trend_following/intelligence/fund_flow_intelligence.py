0c8a0c215deff925093ef45051d0142ca91776af
持羽静尘 <reinhard7717@hotmail.com>
2025-11-05 16:16:56 +0800

    调试

--- strategies/trend_following/intelligence/fund_flow_intelligence.py
+++ strategies/trend_following/intelligence/fund_flow_intelligence.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import numpy as np
 from typing import Dict, List, Tuple, Any
-from strategies.trend_following.utils import get_params_block, get_param_value, normalize_to_bipolar
+from strategies.trend_following.utils import get_params_block, get_param_value, normalize_to_bipolar, bipolar_to_exclusive_unipolar
 
 class FundFlowIntelligence:
     def __init__(self, strategy_instance):
